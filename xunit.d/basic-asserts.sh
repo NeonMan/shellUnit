a66409e07f5752f7243bc6a86a37599498154059
J.Luis <root@heavydeck.net>
2013-11-20 19:19:37 +0100

    Added asserts for lees-than, moved file-related asserts to file-asserts.sh

--- xunit.d/basic-asserts.sh
+++ xunit.d/basic-asserts.sh
@@ -50,17 +50,45 @@ function assertGreaterThan {
 function assertNotGreaterThan {
 	if [ "$1" -gt "$2" ]
 	then
-		fail "$current_test: expect '$2' to NOT be greater than '$1'"
+		fail "$current_test: expect '$2' NOT to be greater than '$1'"
 	else
 		pass
 	fi
 }
 
+#Tests if param 1 is lesser than param 2 (integer)
+#
+# Params:
+#    $1 <-- An integer
+#    $2 <-- An integer
+function assertLessThan {
+        if [ "$1" -lt "$2" ]
+        then
+                pass
+        else
+                fail "$current_test: expect '$2' to be less than '$1'"
+        fi
+}
+
+#Tests if param 1 is not lesser than param 2 (integer)
+#
+# Params:
+#    $1 <-- An integer
+#    $2 <-- An integer
+function assertNotLessThan {
+        if [ "$1" -lt "$2" ]
+        then
+                fail "$current_test: expect '$2' NOT to be less than '$1'"
+        else
+                pass
+        fi
+}
+
 #Tests if param 1 (string) matches the param 2 regexp
 #
 # Params:
-#    $1 <-- A string
-#    $2 <-- A regular expression
+#    $1 <-- A regular expression
+#    $2 <-- A string
 function assertMatches {
 	if [[ "$2" =~ $1 ]]
 	then
@@ -79,22 +107,6 @@ function assertNotMatches {
 	if [[ "$2" =~ $1 ]]
 	then
 		fail "$current_test: expect '$2' to NOT match regular expression '$1'"
-	else
-		pass
-	fi
-}
-
-#Test if a file contains a line matching a regexp
-#
-# Params:
-#    $1 <-- A file
-#    $2 <-- A regular expression
-#
-# @bug test wether the file exists (and is a normal file) to attempt the test
-function assertFileContains {
-	if [[ `grep "$2" $1` = "" ]]
-	then
-		fail "$current_test: expect '$1' to contain '$2'"
 	else
 		pass
 	fi
---
+++ xunit.d/file-asserts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Test if a file contains a line matching a regexp
+#
+# Params:
+#    $1 <-- A file
+#    $2 <-- A regular expression
+#
+# @bug test wether the file exists (and is a normal file) to attempt the test
+function assertFileContains {
+	if [[ `grep "$2" $1` = "" ]]
+	then
+		fail "$current_test: expect '$1' to contain '$2'"
+	else
+		pass
+	fi
+}
