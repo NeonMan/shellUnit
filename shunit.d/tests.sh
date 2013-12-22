fa1f77b2c19aa64859529ca8f11239db6480c678
J.Luis <root@heavydeck.net>
2013-12-22 20:12:47 +0100

    Started github documentation

--- shunit.d/tests.sh
+++
@@ -1,8 +0,0 @@
-numTests=0
-
-#Imcrements the numTests variable
-#
-# Params: none
-countTest () {
-	numTests=`expr $numTests + 1`
-}
--- shunit.d/utils.sh
+++
@@ -1,9 +0,0 @@
-#Normalize path, evaluating and removing the . and .. directories
-#
-# Params:
-#    $1 <-- a path
-# Echoes:
-#    The normalized path
-normalizePath () {
-	echo $1 #$( cd "$( dirname $1 )" && pwd )/$( basename $1 )
-}
