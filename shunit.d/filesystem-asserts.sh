ee08f41470e5546b4dacda722f1388a0b47972cb
J.Luis <root@heavydeck.net>
2013-12-26 01:07:43 +0100

    Added permissions testing

--- shunit.d/filesystem-asserts.sh
+++ shunit.d/filesystem-asserts.sh
@@ -130,3 +130,172 @@ assertNotSameInode () {
 	fi
 }
 
+#Test if a file is owned by a user, either by name or UID
+#
+# Params:
+#     $1 <-- A path
+#     $2 <-- a user name | UID
+assertOwnerIs () {
+	SHU_USER=""
+	if [[ "$2" =~ [0-9][0-9]* ]]
+	then
+		SHU_USER=`shu_userName "$2"`
+	else
+		SHU_USER="$2"
+	fi
+	SHU_FOWNER=`shu_fileOwner "$1"`
+
+	if [ "$SHU_FOWNER" = "$SHU_USER" ]
+	then
+		pass
+	else
+		fail "Expected user <$SHU_USER> but found <$SHU_FOWNER>"
+	fi
+}
+
+#Test if a file is NOT owned by a user, either by name or UID
+#
+# Params:
+#     $1 <-- A path
+#     $2 <-- a user name | UID
+assertOwnerIsNot () {
+	SHU_USER=""
+	if [[ "$2" =~ [0-9][0-9]* ]]
+	then
+		SHU_USER=`shu_userName "$2"`
+	else
+		SHU_USER="$2"
+	fi
+	SHU_FOWNER=`shu_fileOwner "$1"`
+
+	if [ "$SHU_FOWNER" = "$SHU_USER" ]
+	then
+		fail "Expected user not to be <$SHU_USER>"
+	else
+		pass
+	fi
+}
+
+#Test if a file is owned by a group, either by name or GID
+#
+# Params:
+#     $1 <-- A path
+#     $2 <-- a group name | GID
+assertGroupIs () {
+	SHU_GROUP=""
+	if [[ "$2" =~ [0-9][0-9]* ]]
+	then
+		SHU_GROUP=`shu_userGroup "$2"`
+	else
+		SHU_GROUP="$2"
+	fi
+	SHU_FGRP=`shu_fileGroup "$1"`
+
+	if [ "$SHU_FOWNER" = "$SHU_USER" ]
+	then
+		pass
+	else
+		fail "Expected group <$SHU_USER> but found <$SHU_FOWNER>"
+	fi
+}
+
+#Test if a file is owned by a group, either by name or GID
+#
+# Params:
+#     $1 <-- A path
+#     $2 <-- a group name | GID
+assertGroupIsNot () {
+	SHU_GROUP=""
+	if [[ "$2" =~ [0-9][0-9]* ]]
+	then
+		SHU_GROUP=`shu_userGroup "$2"`
+	else
+		SHU_GROUP="$2"
+	fi
+	SHU_FGRP=`shu_fileGroup "$1"`
+
+	if [ "$SHU_FOWNER" = "$SHU_USER" ]
+	then
+		fail "Expected group not to be <$SHU_USER>"
+	else
+		pass
+	fi
+}
+
+#Test if file permissions match
+#
+# Params:
+#     $1 <-- path
+#     $2 <-- permissions
+assertPermissionsAre () {
+	#Check permissions format, either a number, 10-character code, prefix-operand-[rwx] (ToDo)
+	#Numeric permissions
+	if [[ "$2" =~ ^[0-7][0-7][0-7][0-7]? ]]
+	then
+		#Get permissions in numeric format
+		SHV_PERM=`stat --format="%a"  "$1"`
+		if [ "$SHV_PERM" -eq "$1" ]
+		then
+			pass
+		else
+			fail "Permissions are <$SHV_PERM> expected <$1>"
+		fi
+		return
+	fi
+	
+	#Ten-char permissions
+	if [[ "$2" =~ ^[d-][r-][w-][x-][r-][w-][x-][r-][w-][x-] ]]
+	then
+		SHV_PERM=`stat --format="%A"  "$1"`
+		if [ "$SHV_PERM" -eq "$1" ]
+		then
+			pass
+		else
+			fail "Permissions are <$SHV_PERM> expected <$1>"
+		fi
+		return
+	fi
+
+	#fail case
+	fail "Incorrect permission format"
+}
+
+#Test if file permissions don't match
+#
+# Params:
+#     $1 <-- path
+#     $2 <-- permissions
+assertPermissionsAreNot () {
+	#Check permissions format, either a number, 10-character code, prefix-operand-[rwx] (ToDo)
+	SHV_PERMISSIONS=""
+	
+	#Numeric permissions
+	if [[ "$2" =~ ^[0-7][0-7][0-7][0-7]? ]]
+	then
+		#Get permissions in numeric format
+		SHV_PERM=`stat --format="%a"  "$1"`
+		if [ "$SHV_PERM" -eq "$1" ]
+		then
+			fail "Permissions are <$SHV_PERM> expected <$1>"
+		else
+			pass
+		fi
+		return
+	fi
+	
+	#Ten-char permissions
+	if [[ "$2" =~ ^[d-][r-][w-][x-][r-][w-][x-][r-][w-][x-] ]]
+	then
+		SHV_PERM=`stat --format="%A"  "$1"`
+		if [ "$SHV_PERM" -eq "$1" ]
+		then
+			fail "Permissions are <$SHV_PERM> expected <$1>"
+		else
+			pass
+		fi
+		return
+	fi
+
+	#fail case
+	fail "Incorrect permission format"
+}
